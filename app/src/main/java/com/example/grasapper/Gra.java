2e5e957dcbf86babec3e6f1a6df7c628fc46aee2
joannakwiat <62302596+joannakwiat@users.noreply.github.com>
2020-03-25 21:18:36 +0100

    Dodanie 2 ikonek w oknie Gry
    Dodanie 2 ikonek Help, Menu do okna

--- app/src/main/java/com/example/grasapper/Gra.java
+++ app/src/main/java/com/example/grasapper/Gra.java
@@ -1,11 +1,15 @@
 package com.example.grasapper;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Gra extends AppCompatActivity {
-
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
