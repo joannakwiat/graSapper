809e450f9cb984dbe4a047112b9ed941d2a7c86c
joannakwiat <62302596+joannakwiat@users.noreply.github.com>
2020-04-13 14:34:01 +0200

    Dodanie okna kodu + animacja przejścia
    Utworzono nowe okno, gdzie będzie wyświetlany wygenerowany kod. Grafika jest gotowa.
    Dodano animacje przejść, slide-in i out

--- app/src/main/java/com/example/grasapper/Okno_kodu.java
+++ app/src/main/java/com/example/grasapper/Okno_kodu.java
@@ -1,34 +1,36 @@
 package com.example.grasapper;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Gra extends AppCompatActivity {
+public class Okno_kodu extends AppCompatActivity {
     private Button button;
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gra);
-
-        button = (Button) findViewById(R.id.button2);
+        setContentView(R.layout.activity_okno_kodu);
+        button = (Button) findViewById(R.id.back);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openOknoKodu();
+                openGra();
             }
         });
     }
-    public void openOknoKodu(){
-        Intent intent = new Intent(this, Okno_kodu.class);
+
+    public void openGra(){
+        Intent intent = new Intent(this, Gra.class);
         startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    @Override
+    public void finish(){
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
