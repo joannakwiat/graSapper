eaef11d7dc0be38c943f1dd2c793a5e9471ae71d
iwowojno <62302931+iwowojno@users.noreply.github.com>
2020-04-27 20:40:42 +0200

    CoronaEngine 0.1.0

--- app/src/main/java/com/example/grasapper/MainActivity.java
+++ app/src/main/java/com/example/grasapper/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.grasapper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.RequiresApi;
+
+public class MainActivity extends Activity {
+    //pola
     private Button button;
+
+    //metody
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new CoronaEngine(this));
+        /*
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openGra(){
         Intent intent = new Intent(this, Gra.class);
-        startActivity(intent);
+        startActivity(intent)
+
+         */
     }
+
+
 }
