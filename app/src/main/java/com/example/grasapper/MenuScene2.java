359992ee08a4d7328f903d94a5de0351b91b9e5b
kevinbienias <kevinbienias1@gmail.com>
2020-05-31 16:42:52 +0200

    Dodanie resetu i zmiana ikony
    -Obrazek strzalka_skok.png aktualnie nie wygląda jak zawracanie tylko jak skok przez przeszkodę.
    -MenuScene2 dostało nowy przycisk.
    -Dodano ResetSaveTextButton który resetuje stan zapisu gry nie wyłączając się.

--- app/src/main/java/com/example/grasapper/MenuScene2.java
+++ app/src/main/java/com/example/grasapper/MenuScene2.java
@@ -14,16 +14,19 @@ public class MenuScene2 implements Scene
 {
     Sprite background;
     NavigationTextButton toGame;
+    ResetSaveTextButton reset;
     ExitTextButton saveAndExit;
-
+    Context conn;
     @RequiresApi(api = Build.VERSION_CODES.M)
     public MenuScene2(Context context)
     {
+        conn=context;
         Bitmap backgroundImage = BitmapFactory.decodeResource(context.getResources(),R.drawable.manu_screen);
-
         background = new Sprite(backgroundImage, 0,0,1080,1794);
         toGame = new NavigationTextButton(context,"Reasume", 300, 800, R.font.a_b, Color.BLACK, 80, 1000, 7);
-        saveAndExit = new ExitTextButton(context,"Exit the Game", 300, 1000, R.font.a_b, Color.BLACK, 80, 1000);
+        reset = new ResetSaveTextButton(context,"Reset Save", 300, 1000, R.font.a_b, Color.BLACK, 80, 1000);
+        saveAndExit = new ExitTextButton(context,"Exit the Game", 300, 1200, R.font.a_b, Color.BLACK, 80, 1000);
+
     }
 
     @Override
@@ -31,6 +34,7 @@ public class MenuScene2 implements Scene
     {
         toGame.update();
         saveAndExit.update();
+        reset.update(conn);
     }
 
     @Override
@@ -38,6 +42,7 @@ public class MenuScene2 implements Scene
     {
         background.draw(canvas);
         toGame.draw(canvas);
+        reset.draw(canvas);
         saveAndExit.draw(canvas);
     }
 
@@ -46,6 +51,7 @@ public class MenuScene2 implements Scene
     {
         toGame.reciveTouch(event);
         saveAndExit.reciveTouch(event);
+        reset.reciveTouch(event);
     }
 
     @Override
--- app/src/main/java/com/example/grasapper/TextButton.java
+++ app/src/main/java/com/example/grasapper/TextButton.java
@@ -2,6 +2,8 @@ package com.example.grasapper;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
@@ -11,10 +13,14 @@ import android.media.MediaPlayer;
 import android.os.Build;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import androidx.annotation.RequiresApi;
 import androidx.core.content.res.ResourcesCompat;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class TextButton
 {
@@ -262,6 +268,55 @@ class ExitTextButton extends TextButton
         {
             if (clicked) {
                 System.exit(0);
+            }
+        }
+    }
+}
+class ResetSaveTextButton extends TextButton
+{
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    ResetSaveTextButton (Context context, String text, int x, int y, int font, int color, int fontSize, int width)
+    {
+        super(context,text,x,y,font,color,fontSize,width);
+    }
+
+    public void update(Context context)
+    {
+        if (active)
+        {
+            if (clicked) {
+                try {
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("save.txt", Context.MODE_PRIVATE));
+                    outputStreamWriter.write("1");
+                    outputStreamWriter.close();
+                }catch(IOException e) {Log.i("SAVE", "ERROR");}
+                Log.i("SAVE", "Plik nie istnieje!");
+                StartScene.savelevel="1";
+                StartScene.savelevelbomb=1;
+                Bitmap bombImage = BitmapFactory.decodeResource(context.getResources(),R.drawable.bomb_takiczarny);
+                LevelButton bomb2x = new LevelButton(context, bombImage, 437,933,205,  205, 1,2);
+                LevelButton bomb3x = new LevelButton(context, bombImage, 707,933, 205, 205, 1,3);
+                LevelButton bomb4x = new LevelButton(context, bombImage, 167,1198,205, 205, 1,4);
+                LevelButton bomb5x = new LevelButton(context, bombImage, 437,1198,205, 205, 1,5);
+                LevelButton bomb6x = new LevelButton(context, bombImage, 707,1198,205, 205, 1,6);
+                Text text2x=  new Text(context,"2",517,975  ,R.font.a_b,Color.WHITE,97,1020);
+                Text text3x=  new Text(context,"3",787,975  ,R.font.a_b,Color.WHITE,97,1020);
+                Text text4x=  new Text(context,"4",247,1240 ,R.font.a_b,Color.WHITE,97,1020);
+                Text text5x=  new Text(context,"5",517,1240 ,R.font.a_b,Color.WHITE,97,1020);
+                Text text6x=  new Text(context,"6",787,1240 ,R.font.a_b,Color.WHITE,97,1020);
+                StartScene.text2=text2x;
+                StartScene.text3=text3x;
+                StartScene.text4=text4x;
+                StartScene.text5=text5x;
+                StartScene.text6=text6x;
+                StartScene.bomb2=bomb2x;
+                StartScene.bomb3=bomb3x;
+                StartScene.bomb4=bomb4x;
+                StartScene.bomb5=bomb5x;
+                StartScene.bomb6=bomb6x;
+                SceneManager.ACTIVE_SCANE = SceneManager.PREVIOUSE_SCENE;
+                super.update();
+
             }
         }
     }
